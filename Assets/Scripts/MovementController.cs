f5342a867a5b6d7b6014fd06e3b6f18437d5d7db
Maxfirex <35975537+Maxfirex@users.noreply.github.com>
2018-12-01 21:49:51 +0100

    Player changes
    - added player camera
    - added player movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,30 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CyborgController : MonoBehaviour {
+public class MovementController : MonoBehaviour {
 
+    public float speed = 10f;
+    public float jump = 10f;
 
     Animator animator;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        //value for moving forwards and backwards
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        //value for moving left and right
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        animator.speed = speed / 15f;
 
         // Forward run
         bool isRunningForward = Input.GetKey("w");
-        
+
 
         // Left 90 angle run
         bool isRunningLeft = Input.GetKey("a");
-        
+
 
         // Right 90 angle run
         bool isRunningRight = Input.GetKey("d");
-        
+
         // Left 45 angle run
         bool isRunningLeftAngle = (isRunningLeft && isRunningForward) ? true : false;
         animator.SetBool("IsRunningLeftAngle", isRunningLeftAngle);
@@ -45,5 +54,7 @@ public class CyborgController : MonoBehaviour {
             animator.SetBool("IsRunningLeft", false);
             animator.SetBool("IsRunningRight", false);
         }
+
+        transform.Translate(horizontal, 0, vertical);
     }
 }

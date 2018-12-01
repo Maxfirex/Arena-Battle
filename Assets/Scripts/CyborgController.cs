844bc8196b23f472800e5be6fc7b2cf49bf89181
Maxfirex <35975537+Maxfirex@users.noreply.github.com>
2018-12-01 16:41:02 +0100

    Added player
    Player movement animations handled

---
+++ Assets/Scripts/CyborgController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyborgController : MonoBehaviour {
+
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Forward run
+        bool isRunningForward = Input.GetKey("w");
+        
+
+        // Left 90 angle run
+        bool isRunningLeft = Input.GetKey("a");
+        
+
+        // Right 90 angle run
+        bool isRunningRight = Input.GetKey("d");
+        
+        // Left 45 angle run
+        bool isRunningLeftAngle = (isRunningLeft && isRunningForward) ? true : false;
+        animator.SetBool("IsRunningLeftAngle", isRunningLeftAngle);
+        // Right 45 angle run
+        bool isRunningRightAngle = (isRunningRight && isRunningForward) ? true : false;
+        animator.SetBool("IsRunningRightAngle", isRunningRightAngle);
+
+        if (!isRunningLeftAngle && !isRunningRightAngle)
+        {
+            animator.SetBool("IsRunningForward", isRunningForward);
+            animator.SetBool("IsRunningLeft", isRunningLeft);
+            animator.SetBool("IsRunningRight", isRunningRight);
+        }
+        else
+        {
+            animator.SetBool("IsRunningForward", false);
+            animator.SetBool("IsRunningLeft", false);
+            animator.SetBool("IsRunningRight", false);
+        }
+    }
+}
